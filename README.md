911a0927d18afc7a3b0e74f57c9a76a22ecc774a
Cristian Uhrig <cristian.uhrig@wcanvas.com>
2023-02-27 16:43:22 -0300

    Changed config.js for .env file

---
+++ README.md
@@ -0,0 +1,19 @@
+# Telegram Bot integrating OpenAI API, ytdl, and Unsplash API
+
+This is a Telegram bot created using Node.js that integrates the OpenAI API, ytdl, and Unsplash API to provide a variety of features such as image search, video download, and AI-generated text.
+
+## Features
+- Image search
+- MP3 download
+- AI-generated text
+- AI-generated image
+
+## Installation
+- Clone the repository: `git clone https://github.com/UhriG/openai_telegram_bot.git`
+- Install dependencies: `npm install`
+- Rename .env.example to .env and fill in the required fields
+- Run the bot: `npm start`
+
+
+## Author
+- [@UhriG](https://www.github.com/uhrig)
--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const { Configuration, OpenAIApi } = require('openai');
 const ytdl = require('ytdl-core');
 const Unsplash = require('unsplash-js').default;
 const { toJson } = require('unsplash-js');
-const config = require('./config');
+
+// Load environment variables
+require('dotenv').config();
 
 // Configurar la API de Telegram
-const bot = new TelegramBot(config.telegram.token, { polling: true });
+const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
 
 // Configurar la API de OpenAI
 const configuration = new Configuration({
-  apiKey: config.openai.api_key,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
@@ -40,7 +42,7 @@ bot.on('message', async (msg) => {
     bot.sendMessage(chatId, `Descargando ${song.title}`);
     bot.sendAudio(chatId, song.url);
   } else if (message.startsWith('generame una imagen de')) {
-    // Generar una imagen utilizando la API de OpenAI
+    // TODO: Generar una imagen utilizando la API de OpenAI
 
     // For now just tell the user the feature is not yet developed.
     bot.sendMessage(chatId, 'Esta funcionalidad aún no está disponible.');
@@ -48,7 +50,7 @@ bot.on('message', async (msg) => {
     // Buscar una imagen en la web utilizando la API de unsplash
     const query = message.substring(21);
     const unsplash = new Unsplash({
-      accessKey: config.unsplash.access_key,
+      accessKey: process.env.ACCESS_KEY,
     });
     const response = await unsplash.search.photos(query, 1, 1);
     const image = toJson(response).results[0];
